40583b41cb27d8a449b1eeacfbbde53c54bcc901
Isabela Henao <114631428+Gato5002@users.noreply.github.com>
2025-11-29 00:28:51 -0500

    Ejecutable para git Actives

---
+++ test_calculadora.py
@@ -0,0 +1,6 @@
+from Calculadora import sumar, restar, multiplicar, dividir
+
+print("Test suma:", sumar(2, 3))
+print("Test resta:", restar(5, 2))
+print("Test multiplicación:", multiplicar(3, 4))
+print("Test división:", dividir(10, 2))
